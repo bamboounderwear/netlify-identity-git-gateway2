007f291a5557a2e028d3566a6fbf7d026c13f19e
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2024-05-22 21:38:47 -0400

    shorter

--- js/github.js
+++ js/github.js
@@ -1,84 +1,37 @@
 async function listRepoContents(path = '') {
-    let user = netlifyIdentity.currentUser()
-    let token = user.token.access_token
-    var url = "/.netlify/git/github/contents/" + path
-    var bearer = 'Bearer ' + token
-
-    return fetch(url, {
-            method: 'GET',
-            withCredentials: true,
-            credentials: 'include',
-            headers: {
-                'Authorization': bearer,
-                'Content-Type': 'application/json'
-            }
-        }).then(resp => {
-            return resp.json()
-        }).then(data => {
-            if (data.code == 400) {
-                return netlifyIdentity.refresh().then(function(token) {
-                    return listRepoContents(path)
-                })
-            } else {
-                return data
-            }
-        })
-        .catch(error => {
-            return error
-        })
+    return fetchWithAuth("/.netlify/git/github/contents/" + path, {
+        method: 'GET'
+    });
 }
 
 async function listRepoContentsRecursive(path = '') {
-    let contents = await listRepoContents(path)
-    let allContents = []
+    let contents = await listRepoContents(path);
+    let allContents = [];
 
     for (let item of contents) {
-        allContents.push(item)
+        allContents.push(item);
         if (item.type === 'dir') {
-            let subContents = await listRepoContentsRecursive(item.path)
-            allContents = allContents.concat(subContents)
+            let subContents = await listRepoContentsRecursive(item.path);
+            allContents = allContents.concat(subContents);
         }
     }
 
-    return allContents
+    return allContents;
 }
 
 async function getData(mypath = '') {
-    let user = netlifyIdentity.currentUser()
-    let token = user.token.access_token
-    var url = "/.netlify/git/github/contents/" + mypath
-    var bearer = 'Bearer ' + token
-    
-    return fetch(url, {
-            method: 'GET',
-            withCredentials: true,
-            credentials: 'include',
-            headers: {
-                'Authorization': bearer,
-                'Content-Type': 'application/json'
-            }
-        }).then(resp => {
-            return resp.json()
-        }).then(data => {
-            if (data.code == 400) {
-                return netlifyIdentity.refresh().then(function(token) {
-                    return getData(mypath)
-                })
-            } else {
-                // base64 decode content
-                data.content = atob(data.content)
-                return data
-            }
-        })
-        .catch(error => {
-            return error
-        })
+    return fetchWithAuth("/.netlify/git/github/contents/" + mypath, {
+        method: 'GET'
+    }).then(data => {
+        if (data.content) {
+            data.content = atob(data.content);
+        }
+        return data;
+    });
 }
 
 async function saveData(mypath, data, isBinary = false) {
     return getData(mypath).then(function(curfile) {
-        let user = netlifyIdentity.currentUser()
-        let token = user.token.access_token
         let opts = {
             path: mypath,
             message: "initial commit",
@@ -87,33 +40,37 @@ async function saveData(mypath, data, isBinary = false) {
             committer: { name: "Dashpilot", email: "support@dashpilot.com" },
         }
         if (typeof curfile !== 'undefined') {
-            opts.sha = curfile.sha
+            opts.sha = curfile.sha;
         }
 
-        var url = "/.netlify/git/github/contents/" + mypath
-        var bearer = 'Bearer ' + token
-        return fetch(url, {
-                body: JSON.stringify(opts),
-                method: 'PUT',
-                withCredentials: true,
-                credentials: 'include',
-                headers: {
-                    'Authorization': bearer,
-                    'Content-Type': 'application/json'
-                }
-            }).then(resp => {
-                return resp.json()
-            }).then(data => {
-                if (data.code == 400) {
-                    return netlifyIdentity.refresh().then(function(token) {
-                        return saveData(mypath, data, isBinary)
-                    })
-                } else {
-                    return data
-                }
-            })
-            .catch(error => this.setState({
-                message: 'Error: ' + error
-            }))
-    })
+        return fetchWithAuth("/.netlify/git/github/contents/" + mypath, {
+            body: JSON.stringify(opts),
+            method: 'PUT'
+        });
+    });
+}
+
+async function fetchWithAuth(url, options = {}) {
+    let user = netlifyIdentity.currentUser();
+    let token = user.token.access_token;
+    options.headers = {
+        ...options.headers,
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    };
+    options.withCredentials = true;
+    options.credentials = 'include';
+
+    return fetch(url, options).then(resp => resp.json()).then(data => {
+        if (data.code === 400) {
+            return netlifyIdentity.refresh().then(function(token) {
+                return fetchWithAuth(url, options);
+            });
+        } else {
+            return data;
+        }
+    }).catch(error => {
+        console.error(error);
+        throw error;
+    });
 }
